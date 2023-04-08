bad0bcf141b92b58c244fbe9aa8a0a7c0c01714f
lordUhuru <nathanieledeki@gmail.com>
2023-04-08 09:47:20 +0200

    database init works

---
+++ src/main/java/com/codelab/springbatchupdate/SpringBatchUpdateApplication.java
@@ -0,0 +1,13 @@
+package com.codelab.springbatchupdate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBatchUpdateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBatchUpdateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codelab/springbatchupdate/SpringBatchUpdateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codelab.springbatchupdate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBatchUpdateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
