7fee5efc088d5fa34cc8e6e2c43a58861def1d31
lordUhuru <nathanieledeki@gmail.com>
2023-04-16 21:49:34 +0200

    added flyway migration

---
+++ src/main/resources/db/migration/schema/V202304092355__initial_tables.sql
@@ -0,0 +1,10 @@
+CREATE TABLE IF NOT EXISTS customer_order (
+    id uuid not null,
+    order_id uuid not null,
+    customer_id uuid not null,
+    ordered_at timestamp with time zone not null,
+    order_status varchar(255),
+    processed_at timestamp with time zone not null,
+    product_ids text [] not null,
+    UNIQUE(order_id, customer_id)
+);
